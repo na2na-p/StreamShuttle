aeeef73a06773feb5ff9c978ef9deb3d4f7c37fa
na2na-p <na2na@na2na.dev>
2025-10-14 23:15:53 +0900

    Add pytest configuration to set CSRF secret key for testing

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import os
+
+
+def pytest_configure(config):
+    """
+    pytest設定時に実行されるフック関数
+
+    テスト実行前に必要な環境変数を設定します。
+    このフックはpytestの起動時、モジュールのインポート前に実行されます。
+    """
+    os.environ["CSRF_SECRET_KEY"] = "test-secret-key-for-testing-only"
